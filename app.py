492a376f483aea2ccfb25ef541217ad2954ae15b
Quentin Urschfeld <Tishh@users.noreply.huggingface.co>
2023-01-10 13:44:19 +0000

    app commit

---
+++ app.py
@@ -0,0 +1,16 @@
+import gradio as gr
+from fastai.vision.all import *
+
+learn = load_learner('export.pkl')
+
+labels = learn.dls.vocab
+def predict(img):
+    img = PILImage.create(img)
+    pred,pred_idx,probs = learn.predict(img)
+    return {labels[i]: float(probs[i]) for i in range(len(labels))}
+
+title = "find differents type of tank"
+description = "<p style='text-align: center'>Identitifer des tank t72 contre des char leclerc.</p>"
+
+app = gr.Interface(fn=predict, inputs=gr.inputs.Image(shape=(512, 512)), outputs=gr.outputs.Label(num_top_classes=2),title = title)
+app.launch(share=True)
